9fadb3dcdb69639fc2ceeb05d0381d11e33d6648
Bogdan Nikitin <nikitin.bogdan333@yandex.ru>
2024-06-06 23:00:29 +0300

    Docs

---
+++ src/main/javadoc/type-search-index.js
@@ -0,0 +1,1 @@
+typeSearchIndex = [{"l":"All Classes and Interfaces","u":"allclasses-index.html"},{"p":"db","l":"Cache"},{"p":"db","l":"Cache.CacheRecord"},{"p":"db","l":"Main"}];updateSearchResults();
